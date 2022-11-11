d7ff6e5ef908d5979dcf68abdae358386896d5a9
Leonardo Alvarado <leonardoalvarado2013@gmail.com>
2022-11-11 01:14:43 -0400

    EWL APP

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ewl.cr',
+  appName: 'ewlcr',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
